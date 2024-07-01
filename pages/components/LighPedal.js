19f68d30355a89c4c0043d90152698743a39871f
Noah Guardiola <58909117+Blondie-TheManWithNoName@users.noreply.github.com>
2024-07-01 23:47:39 +0200

    refactor into components

---
+++ pages/components/LighPedal.js
@@ -0,0 +1,165 @@
+import React from "react";
+
+import { delay, motion } from "framer-motion";
+import { useEffect, useState } from "react";
+import styles from "../../styles/lightPedal.module.scss";
+import SVGMotion from "./SVGMotion";
+import BikeMotion from "./BikeMotion";
+
+const moveLeft = {
+  initial: { x: 0 },
+  moved: {
+    x: [
+      "0vw",
+
+      "0vw",
+
+      "-100vw",
+      "-100vw",
+
+      "-200vw",
+      "-200vw",
+
+      "-300vw",
+      "-300vw",
+    ], // Array of keyframes
+    transition: {
+      times: [
+        0,
+
+        1 / 6 - 1 / 40,
+        1 / 6 + 1 / 40,
+
+        1 / 2 - 1 / 40,
+        1 / 2 + 1 / 40,
+
+        5 / 6 - 1 / 40,
+        5 / 6 + 1 / 40,
+
+        1,
+      ],
+      duration: 15,
+      ease: "easeInOut",
+      repeat: Infinity,
+    },
+  },
+};
+
+export function LightPedal() {
+  const [width, setWidth] = useState(0);
+  const [heightSVG, setHeightSVG] = useState(0);
+  const [first, setFirst] = useState(0);
+
+  useEffect(() => {
+    const resize = () => {
+      setWidth(window.innerWidth);
+      setHeightSVG(600);
+    };
+    resize();
+    window.addEventListener("resize", resize);
+    return () => window.removeEventListener("resize", resize);
+  }, []);
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setFirst(1);
+    }, 3000);
+
+    return () => clearTimeout(timeout);
+  }, []);
+
+  const time = 15;
+  const r = 150;
+
+  const path = `M0 15
+                H${width - heightSVG / 2}
+                A 1 1, 0, 0 1, ${width - heightSVG / 2} ${heightSVG / 2}
+                H${heightSVG / 2}
+                A 1 1, 0, 0 0, ${heightSVG / 2} ${heightSVG}
+                H${width}`;
+
+  const pathWhite1 = `M${heightSVG / 2} ${heightSVG / 2}
+                      A 1 1, 0, 0 0, ${heightSVG / 2} ${heightSVG}
+                      H0
+                      V${heightSVG / 2}
+                      Z`;
+
+  const path2 = `M0 ${heightSVG} H${width}`;
+
+  const pathWhite2 = `M${width - heightSVG / 1.25} ${heightSVG}
+                      A ${r} ${r}, 0, 0 0, ${width - heightSVG / 1.25 + r} ${
+    heightSVG - r
+  }
+
+  V${heightSVG / 2}
+                      H${width}
+                      V${heightSVG}
+                      Z`;
+  const path3 = `M0 ${heightSVG} 
+                 H${width - heightSVG / 1.25}
+                 A ${r} ${r}, 0, 0 0, ${width - heightSVG / 1.25 + r} ${
+    heightSVG - r
+  }
+                 V${15 + r}
+                 A ${r} ${r}, 0, 0 1, ${width - heightSVG / 1.25 + r * 2} 15
+                 H${width}
+                 `;
+
+  return (
+    <motion.div
+      initial="initial"
+      animate="moved"
+      variants={moveLeft}
+      className={styles.container}
+    >
+      <div className={styles.scene}>
+        <h2>
+          Pedal <br /> Lleuger
+        </h2>
+        <SVGMotion
+          path={path}
+          delay={0}
+          start={first}
+          end={1}
+          extraPath={pathWhite1}
+          heightSVG={heightSVG}
+        />
+        <BikeMotion width={width} />
+      </div>
+
+      <div className={styles.scene}>
+        <section>
+          <p>Gaudeix de les teves rutes en bici sense cap càrrega.</p>
+        </section>
+        <SVGMotion path={path2} delay={time / 6 - 1 / 3} start={0} end={0} />
+      </div>
+
+      <div className={styles.scene}>
+        <section>
+          <p>
+            Pedala i gaudeix del viatge mentre nosaltres et portem les maletes!
+          </p>
+        </section>
+        <SVGMotion
+          path={path3}
+          delay={time / 2 - 1 / 3}
+          start={0}
+          end={0}
+          extraPath={pathWhite2}
+        />
+      </div>
+
+      <div className={styles.scene}>
+        <SVGMotion
+          path={path}
+          delay={(5 / 6) * time - 1 / 3}
+          start={0}
+          end={0}
+        />
+        <h2>
+          Pedal <br /> Lleuger
+        </h2>
+      </div>
+    </motion.div>
+  );
+}
---
+++ pages/components/SVGMotion.js
@@ -0,0 +1,55 @@
+import React from "react";
+import { delay, motion } from "framer-motion";
+const paintPath = {
+  hidden: { pathLength: 0, opacity: 1 },
+  visible: ([delay, start, end]) => {
+    return {
+      pathLength: [start, 1, 1, end, end],
+      transition: {
+        pathLength: {
+          delay,
+          duration: 15,
+          times: [0, 0.1, 0.4, 0.4, 1],
+          ease: "easeInOut",
+          repeat: Infinity,
+        },
+      },
+    };
+  },
+};
+
+const color = "#FF6262";
+const bgColor = "#FFFFFF";
+const strokeWidth = 30;
+
+export default function motionSVG({
+  path,
+  delay,
+  start,
+  end,
+  extraPath,
+  heightSVG,
+}) {
+  return (
+    <motion.svg
+      width="100vw"
+      height={600 + strokeWidth / 2}
+      initial="hidden"
+      animate="visible"
+      key={`path-${start}`}
+      style={{ zIndex: 2 }}
+    >
+      {extraPath && <path d={extraPath} fill={bgColor} />}
+
+      <motion.path
+        d={path}
+        stroke={color}
+        strokeWidth={strokeWidth}
+        strokeLinecap="round"
+        fill="none"
+        variants={paintPath}
+        custom={[delay, start, end, end]}
+      />
+    </motion.svg>
+  );
+}
