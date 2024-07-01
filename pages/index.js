83572df9f7643b66a4406ae95a3ea57ad09e43fa
Noah Guardiola <58909117+Blondie-TheManWithNoName@users.noreply.github.com>
2024-07-01 23:46:51 +0200

    index and sass

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,18 @@
 import { delay, motion } from "framer-motion";
 import { useEffect, useState } from "react";
-import { BikeComponent } from "./components/BikeComponent";
+import { LightPedal } from "./components/LighPedal";
+import title from "../public/title.svg";
+import Image from "next/image";
+import mask from "../public/mask_group.png";
 
 export default function App() {
   return (
-    <>
-      <BikeComponent />
-      <BikeComponent />
-      <BikeComponent />
-    </>
+    <div className="relative">
+      <div className="flex flex-col items-center mt-28">
+        <Image src={title} alt="title" className="scale-125 mt-8 absolute" />
+        <Image src={mask} alt="mask" className="w-screen" />
+      </div>
+      <LightPedal />
+    </div>
   );
 }
